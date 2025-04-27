ea07e3b2bf5e01b1c8167b39d414d47193029932
marcobrunet <marco.brunet@acsoftware.it>
2025-04-27 16:06:27 +0200

    Add test with S3MockExtension

--- S3Connector-service/src/test/java/it/water/connectors/s3/S3ConnectorApiTest.java
+++ S3Connector-service/src/test/java/it/water/connectors/s3/S3ConnectorApiTest.java
@@ -1,4 +1,6 @@
 package it.water.connectors.s3;
+
+import com.adobe.testing.s3mock.junit5.S3MockExtension;
 import it.water.connectors.s3.model.S3ClientConfig;
 import it.water.connectors.s3.service.S3ConnectorFactory;
 import it.water.connectors.s3.service.S3ConnectorSystemServiceImpl;
@@ -6,6 +8,7 @@ import it.water.core.api.service.Service;
 import it.water.core.testing.utils.junit.WaterTestExtension;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.*;
@@ -16,35 +19,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-/**
- * Generated with Water Generator.
- * Test class for S3Connector Services.
- 
-
- */
 @ExtendWith(WaterTestExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class S3ConnectorApiTest implements Service {
+
     private S3ConnectorSystemServiceImpl service;
     private S3Client mockS3Client;
+
+    // QUI: Registro l'estensione correttamente
+    @RegisterExtension
+    static final S3MockExtension s3Mock = S3MockExtension.builder()
+            .withSecureConnection(false)
+            .build();
 
     @BeforeEach
     void setup() {
         mockS3Client = mock(S3Client.class);
 
-        // Creo una dummy S3Configuration
         var config = S3ClientConfig.builder()
                 .accessKey("dummy")
                 .secretKey("dummy")
                 .region("eu-west-1")
                 .bucketName("mock-bucket")
-                .endpoint("http://localhost")
+                .endpoint(s3Mock.getServiceEndpoint().toString()) // <<< usa il mock
                 .build();
 
         service = S3ConnectorFactory.connect(config);
-
-        // Sovrascrivo il vero s3Client con il mock
         service.setS3Client(mockS3Client);
     }
 
